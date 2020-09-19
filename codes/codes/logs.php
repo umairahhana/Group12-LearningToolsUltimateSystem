2f37c1698cb10806b680724d2112026b6b191784
Kevin Lee <kevin@impersuasion.com>
2020-09-19 22:39:35 +0800

    chatroom_UX

--- codes/codes/logs.php
+++ codes/codes/logs.php
@@ -5,7 +5,11 @@
 	$result1 = mysqli_query($con,"SELECT * FROM logs ORDER BY id ASC");
 
 	while($extract = mysqli_fetch_array($result1)) {
-		echo "<span>" . $extract['username'] . "</span>: <span>" . $extract['msg'] . "</span><br />";
+		echo "<span>" . $extract['username'] . "</span>: <span>" . $extract['msg'] . "</span><br/>";
 	}
 
 ?>
+<script>
+	var objDiv = document.getElementById("messages");
+	objDiv.scrollTop = objDiv.scrollHeight;
+</script>
