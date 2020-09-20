74a46f164aa73ed048cd88d44bcb9a3ca37bddae
umairahhana <1161103366@student.mmu.edu.my>
2020-09-20 16:33:18 +0800

    Modify lecture time
    Modifying lecture time clashing error

--- codes/codes/addsub.php
+++ codes/codes/addsub.php
@@ -21,9 +21,19 @@
 					Subject Name: <br>
 					<input type = "text" name = "sub_name" required><br>
 					Lecture Time Start from: <br>
-					<input type = "time" name = "sub_lec_start" required><br>
-					Lecture Time End at: <br>
-					<input type = "time" name = "sub_lec_end" required><br>
+					<input type = "time" id = "sub_lec_start" name = "sub_lec_start" required><br>					
+					Lecture Time End at: <span id = "sub_lec_end" name="sub_lec_end"></span><br>
+					<script>
+						sub_lec_start.oninput = function(){
+							var start = sub_lec_start.value;
+							var splitStart = start.split(":");
+							var value = Number(splitStart[0]) + 2;
+							var time = new Date(0,0,0,value,splitStart[1]).toLocaleTimeString();
+							sub_lec_end.innerHTML = time;
+							sub_lec_end.value = time;
+						};
+			
+					</script>
 					Lecture Class Day: <br>
 					<select name = "sub_lec_day" required>
 						<option value = "Monday">Monday</option>
