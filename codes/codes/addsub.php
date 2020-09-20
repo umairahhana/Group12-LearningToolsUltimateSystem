0c1bcc5dc18d1e88d7d4d3c0074edc638ccadf9d
anfalalhaddi <60769982+anfalalhaddi@users.noreply.github.com>
2020-09-20 18:01:25 +0800

    Revert "Update addsub.php"
    This reverts commit 2db34d766010628376c2a25987f77fd7ebb52024.

--- codes/codes/addsub.php
+++ codes/codes/addsub.php
@@ -60,8 +60,9 @@
 						sub_tut_end.innerHTML = time;
 						sub_tut_end.value = time;
 					};
-				</script>
 
+				</script>
+				
 					Tutorial Class Day: <br>
 					<select name = "sub_tut_day" required>
 						<option value = "Monday">Monday</option>
